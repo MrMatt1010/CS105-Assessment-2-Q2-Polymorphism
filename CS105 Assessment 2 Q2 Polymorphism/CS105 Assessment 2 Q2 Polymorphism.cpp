051d78cd559a3bb48b2fabeb03f1c5893576fb0c
Matt T <mrsymtec@gmail.com>
2023-10-20 17:37:50 +1300

    Beginning of typing code for the assessment. Creating Base Class

--- CS105 Assessment 2 Q2 Polymorphism/CS105 Assessment 2 Q2 Polymorphism.cpp
+++ CS105 Assessment 2 Q2 Polymorphism/CS105 Assessment 2 Q2 Polymorphism.cpp
@@ -3,7 +3,20 @@
 
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#define MAX_SIZE 150
 
+//Video Game base class
+
+class videoGame {
+protected:
+    std::string title; // Stores the title of the game
+    float price;       //Stores the price of the game
+
+public:
+
+};
 
 int main()
 {
