631bc293be804bfe2b5397be4110d88ff1f07f6f
Matt T <mrsymtec@gmail.com>
2023-10-26 09:33:56 +1300

    Changed some code to get the operating system and console type to display. Not working

--- CS105 Assessment 2 Q2 Polymorphism/CS105 Assessment 2 Q2 Polymorphism.cpp
+++ CS105 Assessment 2 Q2 Polymorphism/CS105 Assessment 2 Q2 Polymorphism.cpp
@@ -6,79 +6,64 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#define MAX_SIZE 150
+#define MAX_SIZE 120
 
-//Video Game base class
 
+//vidoeGame Base class
 class videoGame {
 protected:
-    std::string title; // Stores the title of the game
-    float price;       //Stores the price of the game
+    std::string title;
+    float price;
 
 public:
-// Default constructor and array building to store objects entered IE: games and prices
-
     videoGame() : title(""), price(0) {};
-    videoGame(std::string t, float p) : title(t), price(0) {};
-
-// Virtual display method which can be overidden by the dreived classes
-
+    videoGame(std::string t, float p) : title(t), price(p) {};
+    //In here is the default constructor to store objects entered
     void display() {
-        //std::cout << fixed << setPrecision(2);
         std::cout << "Title: " << title << std::endl;
-        std::cout << "Price: " << price << std::endl;
+        std::cout << "Price: " << std::fixed << std::setprecision(2) << price << std::endl;
     }
-
 };
 
-// This is the computerGame Class
-
-class computerGame : public videoGame
-{
+//computerGame class 
+class computerGame : public videoGame {
 private:
     std::string operatingSystem;
+
 public:
+    computerGame(std::string t, float p, std::string o) : videoGame(t, p), operatingSystem(o) {}
 
-//Here is the constructor for the computer system, Function calling the display method of the parent class
-
-    computerGame(std::string t, float p, std::string o) : videoGame(t, p), operatingSystem(o) {};
-    void display()
-    {
-        // calling the display method of the parent class
+    void display() {
         videoGame::display();
         std::cout << "OS Type: " << operatingSystem << std::endl;
     }
 };
 
-// ConsoleGame class deriving from the videogame class
-class consoleGame : public videoGame
-{
+//consoleGame class
+class consoleGame : public videoGame {
 private:
     std::string consoleType;
+
 public:
-    // constructor for the computer system
-    consoleGame(std::string t, float p, std::string c) : videoGame(t, p), consoleType(c) {};
-    void display()
-    {
-        // calling the display method of the parent class
+    consoleGame(std::string t, float p, std::string c) : videoGame(t, p), consoleType(c) {}
+
+    void display() {
         videoGame::display();
         std::cout << "Console Type: " << consoleType << std::endl;
     }
 };
-//This functin displays the list of computer games once it has been created
 
 void displayVideoGames(videoGame** videoGamesList, int size) {
     std::cout << "Video Games List" << std::endl;
     for (int i = 0; i < size; i++) {
         std::cout << "*********************************" << std::endl;
-        videoGamesList[i]->display(); // Use -> to access the display function
+        videoGamesList[i]->display();
         std::cout << "\n*******************************" << std::endl;
     }
 }
 
-int main()
-{
-    videoGame* videoGames[MAX_SIZE]; // Declare videoGames in the main function
+int main() {
+    videoGame* videoGames[MAX_SIZE];
     int currentSize = 0;
 
     char userChoice;
@@ -90,49 +75,45 @@ int main()
 
         std::cout << "Do you want to enter data for a Computer Game or a Console Game (o / c) : ";
         std::cin >> videoGameType;
-        if (videoGameType == 'c')
-        {
+        if (videoGameType == 'c') {
             std::string consoleType;
             std::cout << "Please enter the title of console game: ";
-            
-            (std::cin >> gameTitle);
+            std::cin.ignore();
+            std::getline(std::cin, gameTitle);
             std::cout << "Please enter the price: ";
             std::cin >> price;
             std::cout << "Please enter console type: ";
-           
-            (std::cin >> consoleType);
+            std::cin.ignore();
+            std::getline(std::cin, consoleType);
             consoleGame* newConsoleGame = new consoleGame(gameTitle, price, consoleType);
             videoGames[currentSize++] = newConsoleGame;
         }
         else if (videoGameType == 'o') {
             std::string osType;
             std::cout << "Please enter the title of the computer game: ";
-            (std::cin >>  gameTitle);
+            std::cin.ignore();
+            std::getline(std::cin, gameTitle);
             std::cout << "Please enter the price: ";
             std::cin >> price;
+            std::cin.ignore();
             std::cout << "Please enter the operating system (OS): " << std::endl;
-            (std::cin >>  osType);
+            std::getline(std::cin, osType);
             computerGame* newComputerGame = new computerGame(gameTitle, price, osType);
-            videoGames[currentSize++] = newComputerGame; videoGames[currentSize++] = newComputerGame;
+            videoGames[currentSize++] = newComputerGame;
         }
-        else
-        {
+        else {
             std::cout << "Please enter a valid choice";
         }
-    std::cout << "Do you want to add another game?: ";
-    std::cin >> userChoice;
+
+        std::cout << "Do you want to add another game?: ";
+        std::cin >> userChoice;
     } while (userChoice == 'y' || userChoice == 'Y');
 
     std::cout << std::endl;
     displayVideoGames(videoGames, currentSize);
 
-    // Free the dynamically allocated memory
     for (int i = 0; i < currentSize; i++) {
         delete videoGames[i];
     }
-
-    
-
-    
 }
 
